ae7cc42c262af4fb3ce3a9c4e6a6013981024675
AlexGiannakopoulos <117894640+AlexGiannakopoulos@users.noreply.github.com>
2023-02-28 19:58:40 +0200

    Add files via upload

---
+++ Exercise3OOP/Cars.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Exercise3OOP;
+
+public class Cars
+{
+    public string Num { get; set; }
+    public string Brand { get; set; }
+    public int Km { get; set; }
+    
+    public Cars(string Num, string Brand, int Km)
+    {
+        this.Num = Num;
+        this.Brand = Brand;
+        this.Km = Km;
+    }
+
+    public override string ToString()
+    {
+        return "Car Number: " + Num + " Car brand: " + Brand + " Car Kilometres travelled: " + Km;
+    }
+}
---
+++ Exercise3OOP/Program.cs
@@ -0,0 +1,32 @@
+﻿
+//ask user for car info(number,brand,km)
+//put user inputs in a list
+//imagine 5 cars
+// make a class to add new cars (objects)
+
+using Exercise3OOP;
+
+List<Cars> myList = new List<Cars>();
+
+Console.Write("How many cars listed?: ");
+int listLen = Int32.Parse(Console.ReadLine());
+
+for (int i = 0; i < listLen; i++)
+{
+    Console.Write("Car "+ (i+1) + " number?: ");
+    string num = Console.ReadLine();
+    Console.Write("Car brand?: ");
+    string brand = Console.ReadLine();
+    Console.Write("Car killometers traveled?: ");
+    int km = Int32.Parse(Console.ReadLine());
+    Cars c = new Cars(num, brand, km);
+    
+    myList.Add(c);
+
+    
+}
+foreach (Cars a in myList)
+{
+    Console.WriteLine(a);
+}
+
